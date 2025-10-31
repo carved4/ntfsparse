d9d38374f19287d5e7db3a676db26a8edd9274aa
carved4 <osmith26@adrian.edu>
2025-10-31 06:26:59 -0700

    remove dead code

--- crypto.go
+++ crypto.go
@@ -12,89 +12,45 @@ import (
 
 func extractBootKey(hive *RegistryHive) []byte {
 	keyNames := []string{"JD", "Skew1", "GBG", "Data"}
-	
+
 	var classNames []byte
-	
+
 	for _, keyName := range keyNames {
 		key, err := hive.FindKey("ControlSet001\\Control\\Lsa\\" + keyName)
 		if err != nil {
 			return nil
 		}
-		
+
 		if key.ClassName == "" {
 			return nil
 		}
-		
+
 		classHexStr := ""
 		classBytes := hexStringToBytes(key.ClassName)
-		
+
 		for i := 0; i < len(classBytes); i += 2 {
 			if i < len(classBytes) {
 				classHexStr += string(classBytes[i])
 			}
 		}
-		
+
 		actualBytes := hexStringToBytes(classHexStr)
 		classNames = append(classNames, actualBytes...)
 	}
-	
+
 	if len(classNames) != 16 {
 		return nil
 	}
-	
+
 	scramble := []int{8, 5, 4, 2, 11, 9, 13, 3, 0, 6, 1, 12, 14, 10, 15, 7}
-	
+
 	bootKey := make([]byte, 16)
 	for i := 0; i < 16; i++ {
 		bootKey[i] = classNames[scramble[i]]
 	}
-	
+
 	return bootKey
 }
-
-func extractLSAKey(systemHive *RegistryHive, bootKey []byte) []byte {
-	lsaKey, err := systemHive.FindKey("ControlSet001\\Control\\Lsa")
-	if err != nil {
-		return nil
-	}
-	
-	values := systemHive.GetValues(lsaKey)
-	var encryptedLSAKey []byte
-	
-	for _, vk := range values {
-		if vk.Name == "PolSecretEncryptionKey" {
-			encryptedLSAKey = vk.Data
-			break
-		}
-	}
-	
-	if encryptedLSAKey == nil {
-		for _, vk := range values {
-			if vk.Name == "PolEKList" {
-				encryptedLSAKey = vk.Data
-				break
-			}
-		}
-	}
-	
-	if encryptedLSAKey == nil {
-		for _, vk := range values {
-			if len(vk.Data) >= 60 && (vk.DataType == 3 || vk.DataType == 1) {
-				if vk.Data[0] == 0x00 || vk.Data[4] == 0x00 {
-					encryptedLSAKey = vk.Data
-					break
-				}
-			}
-		}
-	}
-	
-	if encryptedLSAKey == nil || len(encryptedLSAKey) < 28 {
-		return nil
-	}
-	
-	return decryptLSAKeyData(encryptedLSAKey, bootKey)
-}
-
 
 func decryptLSA(value []byte, bootKey []byte, vistaStyle bool) []byte {
 	if vistaStyle {
@@ -104,48 +60,46 @@ func decryptLSA(value []byte, bootKey []byte, vistaStyle bool) []byte {
 		}
 
 		encryptedData := value[28:]
-		
+
 		if len(encryptedData) < 32 {
 			return nil
 		}
-		
+
 		salt := encryptedData[:32]
-		
-		tmpKey := sha256Key(bootKey, salt) 
-		
+
+		tmpKey := sha256Key(bootKey, salt)
+
 		cipherText := encryptedData[32:]
-		
-		plainText := decryptAES(tmpKey, make([]byte, 16), cipherText)  // zero IV
-		
+
+		plainText := decryptAES(tmpKey, make([]byte, 16), cipherText) // zero IV
+
 		if plainText == nil {
 			return nil
 		}
 
-		
 		if len(plainText) < 16 {
 			return nil
 		}
-		
+
 		length := binary.LittleEndian.Uint32(plainText[0:4])
-		
+
 		if len(plainText) < 16+int(length) {
 			return nil
 		}
-		
-		secret := plainText[16:16+length]
-		
-		if len(secret) < 84 { 
+
+		secret := plainText[16 : 16+length]
+
+		if len(secret) < 84 {
 			return nil
 		}
-		
-		lsaKey := secret[52:84]  
-		
+
+		lsaKey := secret[52:84]
+
 		return lsaKey
 	} else {
 		return nil
 	}
 }
-
 
 func sha256Key(key []byte, value []byte) []byte {
 	h := sha256.New()
@@ -164,12 +118,12 @@ func min(a, b int) int {
 }
 
 func decryptLSAKeyData(encryptedLSAKey []byte, bootKey []byte) []byte {
-	return decryptLSA(encryptedLSAKey, bootKey, true) 
+	return decryptLSA(encryptedLSAKey, bootKey, true)
 }
 
 func decryptAES(key, iv, data []byte) []byte {
 	var plainText []byte
-	
+
 	zeroIV := make([]byte, 16)
 	isZeroIV := true
 	for i := 0; i < len(iv) && i < 16; i++ {
@@ -178,23 +132,23 @@ func decryptAES(key, iv, data []byte) []byte {
 			break
 		}
 	}
-	
+
 	if isZeroIV {
 		for index := 0; index < len(data); index += 16 {
 			block, err := aes.NewCipher(key)
 			if err != nil {
 				return nil
 			}
-			
+
 			mode := cipher.NewCBCDecrypter(block, zeroIV)
-			
-			cipherBuffer := data[index:index+16]
+
+			cipherBuffer := data[index : index+16]
 			if len(cipherBuffer) < 16 {
 				padded := make([]byte, 16)
 				copy(padded, cipherBuffer)
 				cipherBuffer = padded
 			}
-			
+
 			decrypted := make([]byte, 16)
 			mode.CryptBlocks(decrypted, cipherBuffer)
 			plainText = append(plainText, decrypted...)
@@ -204,16 +158,16 @@ func decryptAES(key, iv, data []byte) []byte {
 		if err != nil {
 			return nil
 		}
-		
+
 		if len(data)%aes.BlockSize != 0 {
 			return nil
 		}
-		
+
 		mode := cipher.NewCBCDecrypter(block, iv)
 		plainText = make([]byte, len(data))
 		mode.CryptBlocks(plainText, data)
 	}
-	
+
 	return plainText
 }
 
@@ -231,24 +185,24 @@ func decryptSingleDES(key, data []byte) []byte {
 	if err != nil {
 		return nil
 	}
-	
+
 	decrypted := make([]byte, len(data))
 	block.Decrypt(decrypted, data)
-	
+
 	return decrypted
 }
 
 func ridToDESKey(rid uint32, index int) []byte {
 	ridBytes := make([]byte, 4)
 	binary.LittleEndian.PutUint32(ridBytes, rid)
-	
+
 	var s []byte
 	if index == 1 {
 		s = []byte{ridBytes[0], ridBytes[1], ridBytes[2], ridBytes[3], ridBytes[0], ridBytes[1], ridBytes[2]}
 	} else {
 		s = []byte{ridBytes[3], ridBytes[0], ridBytes[1], ridBytes[2], ridBytes[3], ridBytes[0], ridBytes[1]}
 	}
-	
+
 	key := make([]byte, 8)
 	key[0] = s[0] >> 1
 	key[1] = ((s[0] & 0x01) << 6) | (s[1] >> 2)
@@ -258,43 +212,25 @@ func ridToDESKey(rid uint32, index int) []byte {
 	key[5] = ((s[4] & 0x1F) << 2) | (s[5] >> 6)
 	key[6] = ((s[5] & 0x3F) << 1) | (s[6] >> 7)
 	key[7] = s[6] & 0x7F
-	
+
 	for i := 0; i < 8; i++ {
 		key[i] = (key[i] << 1)
 	}
-	
-	return key
-}
 
-func decryptHashWithRID(encryptedHash []byte, rid uint32) []byte {
-	if len(encryptedHash) != 16 {
-		return nil
-	}
-	
-	key1 := ridToDESKey(rid, 1)
-	key2 := ridToDESKey(rid, 2)
-	
-	hash1 := decryptSingleDES(key1, encryptedHash[0:8])
-	hash2 := decryptSingleDES(key2, encryptedHash[8:16])
-	
-	if hash1 == nil || hash2 == nil {
-		return nil
-	}
-	
-	return append(hash1, hash2...)
+	return key
 }
 
 func decryptHashWithBootKey(encryptedHash []byte, bootKey []byte, rid uint32) []byte {
 	if len(encryptedHash) < 16 {
 		return nil
 	}
-	
+
 	if len(encryptedHash) >= 24 {
 		if encryptedHash[0] == 0x02 && encryptedHash[1] == 0x00 {
 			return decryptHashAES(encryptedHash, bootKey, rid)
 		}
 	}
-	
+
 	return decryptHashRC4(encryptedHash, bootKey, rid)
 }
 
@@ -302,54 +238,53 @@ func decryptHashAES(data []byte, bootKey []byte, rid uint32) []byte {
 	if len(data) < 24 {
 		return nil
 	}
-	
+
 	if len(data) < 32 {
 		return make([]byte, 16)
 	}
-	
+
 	salt := data[8:24]
 	encData := data[24:]
-	
+
 	if len(encData) < 16 {
 		return make([]byte, 16)
 	}
-	
+
 	ridBytes := make([]byte, 4)
 	binary.LittleEndian.PutUint32(ridBytes, rid)
-	
+
 	h := sha256.New()
 	h.Write(bootKey)
 	h.Write(ridBytes)
 	h.Write(salt)
 	aesKey := h.Sum(nil)[:16]
-	
+
 	iv := salt[:16]
-	
+
 	decrypted := decryptAES(aesKey, iv, encData[:16])
 	if decrypted == nil {
 		return nil
 	}
-	
+
 	return decrypted
 }
 
 func decryptHashRC4(encryptedHash []byte, bootKey []byte, rid uint32) []byte {
 	ridBytes := make([]byte, 4)
 	binary.LittleEndian.PutUint32(ridBytes, rid)
-	
+
 	h := md5.New()
 	h.Write(bootKey)
 	h.Write(ridBytes)
 	rc4Key := h.Sum(nil)
-	
+
 	cipher, err := rc4.NewCipher(rc4Key)
 	if err != nil {
 		return nil
 	}
-	
+
 	decrypted := make([]byte, len(encryptedHash))
 	cipher.XORKeyStream(decrypted, encryptedHash)
-	
+
 	return decrypted
 }
-
